aadd85162c052e6cc8ad1ab08e73d6c4272d004e
Yashica S <125353032+yashica1511@users.noreply.github.com>
2025-02-16 01:06:48 +0530

    Revert "Backend Structure Created and Admin(HoD) Routes are completed sucessf…"

--- backend/routes/Staff/staff.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const { Login } = require('../../controller/Staff/staff.controller');
-
-routes.post('/login', Login);  
-
-module.exports = routes; 
--- backend/routes/Student/student.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const { Login } = require('../../controller/Student/student.controller');
-
-routes.post('/login', Login);  
-
-module.exports = routes; 
